4174c8f7550cc4e3da886a48b919e298cea2b7a4
akadeepesh <deepesh.kumar.ug21@nsut.ac.in>
2024-10-06 02:50:03 +0530

    Adding vercel json and modifying index js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 
-app.get("/getQuote", async (req, res) => {
+app.get("/api/getQuote", async (req, res) => {
   try {
     const response = await fetch("http://api.quotable.io/random");
     if (!response.ok) {
@@ -15,15 +14,19 @@ app.get("/getQuote", async (req, res) => {
       );
     }
     const data = await response.json();
-
-    // Sending both content and author to the frontend
     res.json({ content: data.content, author: data.author });
   } catch (error) {
-    console.error("Error fetching quote:", error.message); // Log the error message
+    console.error("Error fetching quote:", error.message);
     res.status(500).json({ error: "Could not fetch quote at this time." });
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// For local testing
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
